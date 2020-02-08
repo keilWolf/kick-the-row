0a3c8b73ea3e2b929310cda764418d5e0663eeb6
Wolfram Keil <wkeildev@gmail.com>
2020-02-08 18:53:03 +0100

    add jest for testing

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    verbose: true,
+    moduleFileExtensions: [
+        "test.js",
+        "js",
+        "json",
+        "vue"
+    ],
+    transform: {
+        ".*\\.(vue)$": "vue-jest",
+        "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/components/*.{js,vue}",
+        "!**/node_modules/**"
+    ],
+    coverageReporters: [
+        "html",
+        "text-summary"
+    ],
+}
